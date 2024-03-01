288ecf5e2c9a431370a1370ab2e3a6b8a5afef4e
anshad781 <aaanshad09@gmail.com>
2024-03-01 13:35:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+//specify the elements from HTML
+let indexOneEl = document.getElementById("input1")
+let indexTwoEl = document.getElementById("input2")
+let copyword = document.querySelectorAll(".copy-password")
+
+
+//generate random character
+function randomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+//generate password
+function passwordGenerator() {
+    let indexOne = " "
+    let indexTwo = " "
+    for ( let i = 0 ; i < 15; i++) {
+        indexOne += randomCharacter()
+        indexTwo += randomCharacter()
+    }
+    
+    indexOneEl.textContent = indexOne
+    indexTwoEl.textContent = indexTwo
+    
+}
+
+// Copy to Clipboard
+function copy1()
+{
+    window.getSelection().selectAllChildren(document.getElementById("input1"));
+    document.execCommand("Copy");
+}
+
+function copy2()
+{
+    window.getSelection().selectAllChildren(document.getElementById("input2"));
+    document.execCommand("Copy");
+}
